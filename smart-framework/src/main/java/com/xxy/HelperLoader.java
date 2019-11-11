ef25664450805dc620755e8766f0d3529074f8ce
xiangxy26878 <xiangxy26878@hundsun.com>
2019-11-11 18:03:16 +0800

    加载Controller

---
+++ smart-framework/src/main/java/com/xxy/HelperLoader.java
@@ -0,0 +1,20 @@
+package com.xxy;
+
+import com.xxy.util.*;
+
+/**
+ * 加载相应的Helper类
+ */
+public final class HelperLoader {
+    public static void init(){
+        Class<?>[] classList = {
+                ClassHelper.class,
+                BeanHelper.class,
+                IocHelper.class,
+                ControllerHelper.class
+        };
+        for(Class<?> cls : classList){
+            ClassUtil.loadClass(cls.getName(),true);
+        }
+    }
+}
